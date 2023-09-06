c42b9a6df6c8d99fca2dfb1dcb1e2be35c4bfd9d
lukas <lukas.kaufm@outlook.com>
2023-09-06 11:27:27 +0200

    move Record class

--- cli/src/main/kotlin/ls/backup/cli/Backup.kt
+++ cli/src/main/kotlin/ls/backup/cli/Backup.kt
@@ -1,15 +1,14 @@
 package ls.backup.cli
 
-import aws.sdk.kotlin.runtime.auth.credentials.StaticCredentialsProvider
 import aws.sdk.kotlin.services.s3.S3Client
 import aws.sdk.kotlin.services.s3.headBucket
 import aws.sdk.kotlin.services.s3.listObjectsV2
 import aws.sdk.kotlin.services.s3.model.GetObjectRequest
-import aws.smithy.kotlin.runtime.auth.awscredentials.Credentials
 import aws.smithy.kotlin.runtime.content.writeToFile
 import aws.smithy.kotlin.runtime.net.Url
 import kotlinx.coroutines.runBlocking
 import ls.kafka.model.DumpRecord
+import ls.kafka.model.Record
 import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
@@ -69,39 +68,6 @@ class BackupFile(name: String, inputStream: InputStream) {
 
 
 fun DumpRecord.withTopic(topic: String) = Record(topic, partition, offset, ts, key, value)
-
-data class Record(
-    val topic: String,
-    val partition: Int,
-    val offset: Long,
-    val ts: Long,
-    val key: ByteArray,
-    val value: ByteArray
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Record
-
-        if (topic != other.topic) return false
-        if (partition != other.partition) return false
-        if (offset != other.offset) return false
-        if (ts != other.ts) return false
-        if (!key.contentEquals(other.key)) return false
-        return value.contentEquals(other.value)
-    }
-
-    override fun hashCode(): Int {
-        var result = topic.hashCode()
-        result = 31 * result + partition
-        result = 31 * result + offset.hashCode()
-        result = 31 * result + ts.hashCode()
-        result = 31 * result + key.contentHashCode()
-        result = 31 * result + value.contentHashCode()
-        return result
-    }
-}
 
 fun InputStream.loadDump(): Sequence<DumpRecord> {
     val dis = DataInputStream(this)
